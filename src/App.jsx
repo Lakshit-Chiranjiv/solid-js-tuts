3b89d39d22302c7acb2351be531790f40707b644
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-08 23:15:40 +0530

    dynamic content and fragments

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import styles from './App.module.css';
 
 function App() {
 
+  const name = "Lakshit"
+  const element = (<ul>
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </ul>)
+
   return (
     <div className={styles.App}>
       <h1 className={styles.heading}>SOLID JS</h1>
       {/* static text */}
-      <div className={styles.staticTxt}>
+      <div className={styles.box1}>
         <h1>Static Text</h1>
-        <h3>Hello Solid JS</h3>
+        <h3>Hello Solid JS</h3> 
+      </div>
+
+      {/* dynamic text & content | fragments */}
+      <div className={styles.box2}>
+        <h1>Dynamic text & content | Fragments</h1>
+        <>
+          <h3>My name is {name}</h3>
+          {element}
+        </>
       </div>
 
     </div>
