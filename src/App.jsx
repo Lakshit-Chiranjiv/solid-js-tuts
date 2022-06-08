ef79bdd92b275f835cd84d49660cd7a65b5edb4b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-08 22:58:27 +0530

    static text docced

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
+
   return (
     <div class={styles.App}>
-      <h1>Hello Solid JS</h1>
+      {/* static text */}
+      <h1>Static Text</h1>
+      <h3>Hello Solid JS</h3>
+
     </div>
   );
 }
